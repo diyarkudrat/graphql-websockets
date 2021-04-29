e0be6422e079d82f2996604e129c3842597caee9
Diyar Kudrat <dkudrat@gmail.com>
2021-04-29 16:53:03 -0700

    initial commit

---
+++ client/index.js
@@ -0,0 +1,9 @@
+// Get references to DOM elements
+const sendBtn = document.querySelector('#send');
+const messages = document.querySelector('#messages');
+const messageInput = document.querySelector('#message-input');
+
+let ws;
+
+
+  
