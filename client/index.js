2487a376f9348462c74182e3e3a447de1d269392
Diyar Kudrat <dkudrat@gmail.com>
2021-05-10 21:51:08 -0700

    handle displaying messages with the websockets

--- client/index.js
+++ client/index.js
@@ -5,5 +5,38 @@ const messageInput = document.querySelector('#message-input');
 
 let ws;
 
+// Display messages from websocket
+function showMessage(msg) {
+    messages.innerHTML += `${msg}\n\n`;
+    messages.scrollTop = messages.scrollHeight;
+    messageInput.value = '';
+}
 
-  
+function init() {
+    // Clean up before restarting websocket connection
+    if (ws) {
+        ws.onerror = ws.onopen = ws.onclose = null;
+        ws.close();
+    }
+
+    es = new WebSocket('ws://localhost:6969');
+
+    ws.onopen = () => console.log('Connection Opened!!');
+
+    ws.onmessage = () => showMessage(JSON.parse(e.data));
+
+    ws.onclose = () => ws = null;
+}
+
+// Handle Button clicks
+sendBtn.onclick = function () {
+    if (!ws) {
+        showMessage("No WebSocket Connection");
+        return;
+    }
+
+    ws.send(messageInput.value);
+    showMessage(messageInput.value);
+}
+
+init();
